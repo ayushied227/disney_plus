1863926316b32eb0b1259ffe65e4b6bf4dc021b4
Ayushi <ayushidufare22@gmail.com>
2022-12-23 18:13:23 +0530

    react-redux-firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyANvZsc6aT0NBws5j5tds5d76GxtOYXlfU",
+    authDomain: "disneyplus-clone-227.firebaseapp.com",
+    projectId: "disneyplus-clone-227",
+    storageBucket: "disneyplus-clone-227.appspot.com",
+    messagingSenderId: "121630679876",
+    appId: "1:121630679876:web:058192ddc10322332cad11",
+    measurementId: "G-JLZ6FPBEL3"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
